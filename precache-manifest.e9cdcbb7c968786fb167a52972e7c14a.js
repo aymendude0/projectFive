d40d0daba422822ae6e4b6edf61b76558ca067ae
Aymen <aymenm@ive.com>
2021-08-25 21:05:45 -0400

    Updates

---
+++ precache-manifest.e9cdcbb7c968786fb167a52972e7c14a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7712fc5ac6457a8cadfded3f82612cb",
+    "url": "/projectFive/index.html"
+  },
+  {
+    "revision": "3091995c9d773fefd504",
+    "url": "/projectFive/static/css/main.d34f8832.chunk.css"
+  },
+  {
+    "revision": "2f1b87a9f5d9a4e22a62",
+    "url": "/projectFive/static/js/2.d7f5cf4d.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/projectFive/static/js/2.d7f5cf4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3091995c9d773fefd504",
+    "url": "/projectFive/static/js/main.c8503052.chunk.js"
+  },
+  {
+    "revision": "3fdbc7344147fd0c59ef",
+    "url": "/projectFive/static/js/runtime-main.1bdd1428.js"
+  },
+  {
+    "revision": "a485c0ea5cfb26eb9f813f7cf84d1a0f",
+    "url": "/projectFive/static/media/0d3e4a84ba9f77eef545736731c9e2a8.a485c0ea.jpg"
+  }
+]);
